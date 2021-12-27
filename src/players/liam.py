9b60fd0e42f7d6f9f328ab107241c613e3f4c076
Liam Whelan <liamwhelan08115@gmail.com>
2021-12-27 13:10:54 -0700

    Improved LiamBot

--- src/players/liam.py
+++ src/players/liam.py
@@ -4,4 +4,30 @@ class LiamBot:
         self.name = "Liam"
 
     def decideOnCard(self, card, tokens, game_state):
-        return False
+        my_cards = game_state.player_cards[self.name]
+        my_tokens = game_state.player_tokens[self.name]
+        other_cards = self.get_other_cards(game_state)
+        if card <= 5:
+            return True
+        elif tokens > 10:
+            return True
+
+        elif card > 5:
+            for cards in my_cards:
+                if cards +- 2 == card:
+                    for other_card in other_cards:
+                        if other_card +- 1 == card:
+                            return False
+                        else:
+                            if tokens >= 8:
+                                return True
+                            else:
+                                return False
+
+    def get_other_cards(self, game_state):
+        other_cards = []
+        for player, cards in game_state.player_cards.items():
+            if player != self.name:
+                other_cards = other_cards + cards
+        return other_cards
+
